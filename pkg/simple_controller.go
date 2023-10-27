227dcbcda566318a93aa17e29d93ad24fe665682
nameof <constexpr@foxmail.com>
2023-10-27 16:00:48 +0800

    解决service删除annotation未删除ingressbug

--- pkg/simple_controller.go
+++ pkg/simple_controller.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	v1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
@@ -82,36 +83,55 @@ func (c *SimpleController) ServiceAddOrUpdate(obj interface{}) {
 	value, ok := service.GetAnnotations()[exposeIngressKey]
 	if ok && "true" == value {
 		c.syncIngerss(namespace, name)
-		return
+	} else {
+		c.deleteIngerss(namespace, name)
 	}
 	log.Printf("ignore service: %s\n", key)
 }
 
 func (c *SimpleController) syncIngerss(namespace string, name string) {
 	log.Printf("check and create/update ingress")
-	// TODO
+	ingress := c.getIngressByService(namespace, name)
+	if ingress != nil {
+		return
+	}
+
+	// TODO创建Ingress
+}
+
+func (c *SimpleController) deleteIngerss(namespace string, name string) {
+	log.Printf("check and create/update ingress")
+	ingress := c.getIngressByService(namespace, name)
+	if ingress != nil {
+		log.Printf("delete ingress %s\n", ingress.GetName())
+		err := c.client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), ingress.GetName(), metav1.DeleteOptions{})
+		if err != nil {
+			log.Printf("error delete ingress %s %s\n", ingress.GetName(), err)
+		}
+	}
 }
 
 func (c *SimpleController) ServiceDelete(obj interface{}) {
 	key, _ := cache.MetaNamespaceKeyFunc(obj)
 	log.Printf("ServiceDelete：%s\n", key)
 	namespace, name, _ := cache.SplitMetaNamespaceKey(key)
+	c.deleteIngerss(namespace, name)
+}
+
+func (c *SimpleController) getIngressByService(namespace string, name string) *v1.Ingress {
 	ingresses, err := c.ingressLister.Ingresses(namespace).List(labels.Everything())
 	if err != nil {
 		log.Printf("error List All Ingress %s\n", err)
-		return
+		return nil
 	}
+
 	for _, ingress := range ingresses {
 		value, ok := ingress.GetAnnotations()[ownerServiceNameKey]
 		if ok && value == name {
-			log.Printf("delete ingress %s\n", ingress.GetName())
-			err = c.client.NetworkingV1().Ingresses(namespace).Delete(context.TODO(), ingress.GetName(), metav1.DeleteOptions{})
-			if err != nil {
-				log.Printf("error delete ingress %s %s\n", ingress.GetName(), err)
-				continue
-			}
+			return ingress
 		}
 	}
+	return nil
 }
 
 func IngressDelete(obj interface{}) {
